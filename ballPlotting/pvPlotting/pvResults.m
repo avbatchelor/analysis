ede2c4d86d22c6da36ff8ec62b9d22051b54dd1a
avbatchelor <alexbatchelor@hotmail.co.uk>
2017-12-06 18:07:40 -0500

    plotting pv vs freq

---
+++ ballPlotting/pvPlotting/pvResults.m
@@ -0,0 +1,23 @@
+% Plot particle velocity 
+
+%% Data
+freq = [100,140,200,225,300,500,800];
+speaker1 = [1.2653,1.2475,1.2699,1.2952,1.2607,1.2549,1.2682];
+speaker2 = [1.2506,1.2212,1.2513,1.2593,1.235,1.28,1.2092];
+speaker3 = [1.2538,1.2483,1.2242,1.2493,1.2432,1.2037,1.2131];
+
+%% Figure Settings 
+colorSet = distinguishable_colors(3,'w');
+
+%% Figure
+figure;
+hold on 
+plot(freq,speaker1,'Marker','*','Color',colorSet(1,:),'Linewidth',2)
+plot(freq,speaker2,'Marker','*','Color',colorSet(2,:),'Linewidth',2)
+plot(freq,speaker3,'Marker','*','Color',colorSet(3,:),'Linewidth',2)
+
+ylabel('Particle velocity (mm/s)')
+xlabel('Frequency')
+ylim([0 1.5])
+
+legend({'speaker 1','speaker 2','speaker 3'},'Location','best')
