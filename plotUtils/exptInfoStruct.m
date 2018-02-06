2686d6534dfb25c9c7aee9da639a1c071b8d93f0
avbatchelor <abatchelor@fas.harvard.edu>
2018-02-06 18:07:29 -0500

    exptInfoStruct

---
+++ plotUtils/exptInfoStruct.m
@@ -0,0 +1,6 @@
+function exptInfo = exptInfoStruct(prefixCode,expNum,flyNum,flyExpNum)
+
+exptInfo.prefixCode = prefixCode; 
+exptInfo.expNum = expNum; 
+exptInfo.flyNum = flyNum; 
+exptInfo.flyExpNum = flyExpNum; 
